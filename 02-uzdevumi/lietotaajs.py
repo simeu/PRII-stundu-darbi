2bd0920e4fa919d58370327907b6c0aa02726040
simeu <71383052+simeu@users.noreply.github.com>
2022-10-19 11:12:34 +0300

    19.10

--- 02-uzdevumi/lietotaajs.py
+++ 02-uzdevumi/lietotaajs.py
@@ -1,4 +1,5 @@
 import json
+from fpdf import FPDF
 
 class Lietotajs:
     def __init__(self, vards, uzvards, vecums, valoda, pieteiksanos_skaits=0):
@@ -19,6 +20,28 @@ class Lietotajs:
 
     def atiestatit_pieteiksanos_skaitu(self):
         self.pieteiksanos_skaits = 0
+
+    def veido_vardnicu(self):
+        dati = self.__dict__
+        return dati
+
+    def saglabat_json(self):
+        dati = self.veido_vardnicu()
+        datiJSON = json.dumps(dati)
+        nosaukums = f'{self.vards}-{self.uzvards}-{self.valoda}.json'
+        with open(nosaukums, "w", encoding="utf-8") as f:
+            f.write(datiJSON)
+
+    def saglabat_pdf(self):
+        pdf = FPDF()
+        pdf.add_page()
+        pdf.set_font("Arial", size=12)
+        pdf.cell(200, 10, txt="Lietotaji", ln=1, align="C")
+        pdf.cell(200, 10, txt="Lietotajvārds: " + self.vards, ln=1, align="L")
+        pdf.cell(200, 10, txt="Vards, Uzvards: " + self.uzvards, ln=1, align="L")
+        pdf.cell(200, 10, txt="Vecums: " + str(self.vecums), ln=1, align="L")
+        pdf.cell(200, 10, txt="Valoda: " + self.valoda, ln=1, align="L")
+        pdf.output("lietotajs.pdf")
 
 nikola = Lietotajs ("Nikola", "Skuja", 17, "latviešu")
 druvis = Lietotajs ("Druvis", "Liepa", 34,"angļu")
@@ -53,16 +76,7 @@ class Admin(Lietotajs):
     def paradit_privilegijas(self):
         print(f"{self.privilegijas}")
 
-    def saglabat_json(self):
-        dati = self.veido_vardnicu()
-        datiJSON = json.dumps(dati)
-        nosauk = f"{self.razotajs}-{self.modelis}-{self.gads}"
-        with open("auto.json", "w", encoding="utf-8") as f:
-            f.write(datiJSON)
 
-katrina = Admin("Katrīna", "Ločmele", "17", "latviešu")
+# katrina = Admin("Katrīna", "Ločmele", "17", "latviešu")
 #katrina.paradit_privilegijas()
-print(katrina.privilegijas.paradit_privilegijas())
-
-
-
+# print(katrina.privilegijas.paradit_privilegijas())
--- 02-uzdevumi/lietotajsgui.py
+++ 02-uzdevumi/lietotajsgui.py
@@ -1,20 +1,37 @@
 import PySimpleGUI as sg
 import lietotaajs as li
+from fpdf import FPDF
 
 sg.theme("Black")
 
-izkartojums = [
-[sg.Text("Laipni lūgts mūsu nezināmajā portālā!")],
-[sg.Text("Ievadiet savu lietotājvārdu"), sg.Input(key = "-USER-")],
+izvl = [
+    ["File", ["Saglabāt kā JSON", "Saglabāt kā PDF"]],
+    ["Help", ["About", "Support"]]
+]
+izk1 = [
+[sg.Text("Laipni lugts musu nezinamajā portala!")],
+[sg.Text("Ievadiet savu lietotajvardu"), sg.Input(key = "-USER-")],
+[sg.Text("Ievadiet savu vardu un uzvardu"), sg.Input(key = "-USER2-")],
 [sg.Text("Ievadiet savu vecumu"), sg.Input(key = "-T-")],
-[sg.Text("Ievadiet savu izvēlēto valodu"), sg.Input(key = "-P-")],
+[sg.Text("Ievadiet savu izveleto valodu"), sg.Input(key = "-P-")],
 [sg.Button("OK"), sg.Text(key = "-X-")],
 [sg.Button("Reģistrēt"), sg.Text(key = "-Z-")],
-[sg.Button("Iziet"), sg.Text(key = "-M-")]
+[sg.Button("Iziet"), sg.Text(key = "-M-")],]
+
+izk2 = [[sg.Button("Saglabāt kā JSON", size = 12)]]
+
+izk3 = [[sg.Button("Saglabāt kā PDF", size = 12)]]
+
+izk4 = [
+    [sg.Button("Apraksts", size = 12)]
+]
+izkartojums = [
+    [sg.Menu(izvl)],
+    [sg.TabGroup([[sg.Tab("Ievade", izk1)],[sg.Tab("Saglabāt kā JSON", izk2)],[sg.Tab("Saglabāt kā PDF", izk3)], sg.Tab("Apraksts", izk4)])]
+
 ]
 
 logs = sg.Window("Nezināmās programmas ierakstīšanās", izkartojums)
-
 while True:
     notikums, vertibas = logs.read()
     if notikums == sg.WINDOW_CLOSED:
@@ -24,13 +41,27 @@ while True:
         logs.close()
     if notikums == "Reģistrēt":
         vards = vertibas["-USER-"]
+        uzvards = vertibas["-USER2-"]
         vecums = vertibas["-T-"]
         valoda = vertibas["-P-"]
-
-        lietotajs = li.Lietotajs(vards, vecums, valoda)
-        lietotajs.paradit()
+        lietotajs = li.Lietotajs(vards, uzvards, vecums, valoda)
+        lietotajs.sasveicinies()
         print("Dati reģistrēti")
 
     if notikums == "Iziet":
         logs.close()
 
+    if notikums == "Saglabāt kā JSON":
+        lietotajs.veido_vardnicu()
+        lietotajs.saglabat_json()
+        lietotajs.sasveicinies()
+
+    if notikums == "Saglabāt kā PDF":
+        lietotajs.veido_vardnicu()
+        lietotajs.saglabat_pdf() # Rāda error'u "Lietotajs is not defined", kaut vai ja tas ir definēts.
+        lietotajs.sasveicinies()
+
+    if notikums == "About":
+        sg.popup("Hello, World!")
+        if notikums == "OK":
+            logs.close
